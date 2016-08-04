da7d82d597461aa9273ef807cf75b8e50c25d23c
Tyler Hasman <tylerhasman10@hotmail.com>
2016-08-04 12:02:44 -0700

    Removed info

--- src/net/Start.java
+++ src/net/Start.java
@@ -7,10 +7,6 @@ import java.util.Scanner;
 import net.socket.SocketConnectionFactory;
 
 public class Start {
-	//46.105.54.50
-	//158.69.123.139 - MapleDestiny
-	//Remote Server 149.56.15.132
-
 	public static void main(String[] args) throws IOException {
 		
 		try(Scanner scanner = new Scanner(System.in)){
