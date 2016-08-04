0b3bb2daf613206ccdf88e9b302063962411beae
Tyler <tylerhasman10@hotmail.com>
2016-08-04 11:45:06 -0700

    Added prompt for target ip

--- src/net/Start.java
+++ src/net/Start.java
@@ -2,6 +2,8 @@ package net;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.Scanner;
+
 import net.socket.SocketConnectionFactory;
 
 public class Start {
@@ -11,26 +13,29 @@ public class Start {
 
 	public static void main(String[] args) throws IOException {
 		
-		String localHost = args[0];
-		
-		System.out.println("Using local host "+localHost);
-		
-		String host = "localhost";
-		
-		SocketConnectionFactory scf = new SocketConnectionFactory();
-		
-		scf.startAcceptingConnectionsFrom(8480, InetAddress.getByName(host), 8484);
-		
-		while(true){
-			Proxy proxy = new Proxy(scf, localHost);
+		try(Scanner scanner = new Scanner(System.in)){
 			
-			proxy.acceptClient();
 			
-			Thread thread = new Thread(proxy);
-			thread.setDaemon(true);
-			thread.start();
+			String localHost = "localhost";
+			
+			System.out.println("Enter the ip to forward to: ");
+			
+			String host = scanner.nextLine();
+			
+			SocketConnectionFactory scf = new SocketConnectionFactory();
+			
+			scf.startAcceptingConnectionsFrom(8480, InetAddress.getByName(host), 8484);
+			
+			while(true){
+				Proxy proxy = new Proxy(scf, localHost);
+				
+				proxy.acceptClient();
+				
+				Thread thread = new Thread(proxy);
+				thread.setDaemon(true);
+				thread.start();
+			}
 		}
-		
 		
 	}
 	
